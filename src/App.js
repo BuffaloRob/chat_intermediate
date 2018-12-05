13a3a4ef981ac6736251515e67d0bab3c74cf305
buffalorob <peckham.rob@gmail.com>
2018-12-05 09:07:31 -0500

    +- ThreadTabs to work w/ Tabs

--- src/App.js
+++ src/App.js
@@ -138,27 +138,18 @@ const Tabs = props => (
 )
 
 class ThreadTabs extends React.Component {
-  handleClick = (id) => {
-    store.dispatch({
-      type: 'OPEN_THREAD',
-      id: id,
-    });
-  };
 
   render() {
-    const tabs = this.props.tabs.map((tab, index) => (
-      <div
-        key={index}
-        className={tab.active ? 'active item' : 'item'}
-        onClick={() => this.handleClick(tab.id)}
-      >
-        {tab.title}
-      </div>
-    ))
     return (
-      <div className='ui top attached tabular menu'>
-        {tabs}
-      </div>
+      <Tabs 
+        tabs={this.props.tabs}
+        onClick={(id) => (
+          store.dispatch({
+            type: 'OPEN_THREAD',
+            id: id,
+          })
+        )}
+      />
     );
   }
 }
